2e9c1cefcdabcfcbf0a842750c73e3b4d01fa2b8
Dimitrios Maragkos <34100964+Jimmys20@users.noreply.github.com>
2023-03-23 01:14:08 +0200

    commit

---
+++ SO75817635/Models/EmployeeInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace SO75817635.Models
+{
+    public class EmployeeInfo
+    {
+        public int EmployeeInfoId { get; set; }
+        public string? SignatureField { get; set; } = null!;
+    }
+}
